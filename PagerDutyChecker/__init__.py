14856e381686fba9c8776f5da02f02e2175c9c86
Daniel Carrillo <daniel.carrillo@gmail.com>
2020-09-04 17:43:16 +0200

    Refactor

--- PagerDutyChecker/__init__.py
+++ PagerDutyChecker/__init__.py
@@ -1,6 +1,10 @@
+import webbrowser
 from os import path
+from typing import Dict, List
 
 from PySide2 import QtCore, QtGui, QtWidgets
+
+import pdpyras
 
 
 BASEDIR, _ = path.split(path.realpath(__file__))
@@ -12,12 +16,14 @@ class Resources():
     OK_ICON = path.join(BASEDIR, '../resources/pd_ok2.png')
 
 
-class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
-    def __init__(self, parent=None):
+class TrayIcon(QtWidgets.QSystemTrayIcon):
+    def __init__(self, parent=None, conf: Dict = dict()):
         QtWidgets.QSystemTrayIcon.__init__(self, QtGui.QIcon(Resources.OK_ICON), parent)
         self.setToolTip('PagerDuty Check')
+        self.conf = conf
         self.menu = QtWidgets.QMenu(parent)
         self.add_exit_menu_item()
+        self.update_incidents()
 
     def add_exit_menu_item(self) -> None:
         self.menu.addSeparator()
@@ -25,9 +31,40 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
         exit_item.triggered.connect(self.exit)
         self.setContextMenu(self.menu)
 
+    def update_incidents(self) -> None:
+        incidents = self._check_pager_duty()
+
+        if incidents:
+            icon = QtGui.QIcon(Resources.OK_ICON)
+            incidents.sort(key=lambda k: k['status'])
+
+            self.menu.clear()
+            for incident in incidents:
+                print(incident)
+                if incident['status'] == 'acknowledged':
+                    icon = QtGui.QIcon(Resources.ACK_ICON)
+                else:
+                    icon = QtGui.QIcon(Resources.ALERT_ICON)
+
+                action = self.menu.addAction(icon, f'{incident["created_at"]} {incident["title"]}')
+                action.triggered.connect(
+                    lambda f=webbrowser.open, url=incident['html_url']: webbrowser.open(url)
+                )
+
+            self.add_exit_menu_item()
+            self.setIcon(icon)
+
+    def _check_pager_duty(self) -> List:
+        pd_session = pdpyras.APISession(self.conf['pd_api_key'])
+        incidents = pd_session.list_all('incidents',
+                                        params={
+                                            'team_ids[]': self.conf['pd_teams'],
+                                            'user_ids[]': self.conf['pd_users'],
+                                            'statuses[]': ['triggered', 'acknowledged']
+                                        })
+
+        return incidents
+
     def exit(self) -> None:
         self.hide()
         QtCore.QCoreApplication.exit()
-
-
-class Check():
--- pdcheck.py
+++ pdcheck.py
@@ -1,45 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
-import webbrowser
 from os import getenv, path
-from typing import Dict
 
 import PagerDutyChecker as pdc
 
-from PySide2 import QtCore, QtGui, QtWidgets
-
-import pdpyras
+from PySide2 import QtCore, QtWidgets
 
 import yaml
-
-
-def check_pager_duty(*, tray_icon: QtWidgets.QSystemTrayIcon, conf: Dict) -> None:
-    pd_session = pdpyras.APISession(conf['pd_api_key'])
-    incidents = pd_session.list_all('incidents',
-                                    params={
-                                        'team_ids[]': conf['pd_teams'],
-                                        'user_ids[]': conf['pd_users'],
-                                        'statuses[]': ['triggered', 'acknowledged']
-                                    })
-    if incidents:
-        tray_icon.menu.clear()
-        icon = QtGui.QIcon(pdc.Resources.OK_ICON)
-        incidents.sort(key=lambda k: k['status'])
-        for incident in incidents:
-            print(incident)
-            if incident['status'] == 'acknowledged':
-                icon = QtGui.QIcon(pdc.Resources.ACK_ICON)
-            else:
-                icon = QtGui.QIcon(pdc.Resources.ALERT_ICON)
-
-            action = tray_icon.menu.addAction(icon, f'{incident["created_at"]} {incident["title"]}')
-            action.triggered.connect(
-                lambda f=webbrowser.open, url=incident['html_url']: webbrowser.open(url)
-            )
-
-        tray_icon.add_exit_menu_item()
-        QtWidgets.QSystemTrayIcon.setIcon(tray_icon, icon)
 
 
 def main():
@@ -61,20 +29,17 @@ def main():
             raise ValueError('Pagerduty API key has to be provided')
 
     except (IOError, yaml.YAMLError, ValueError) as e:
-        print(f'Error reading config file {conf_file}:\n{e}')
+        print(e)
         sys.exit(1)
 
     app = QtWidgets.QApplication(sys.argv)
     widget = QtWidgets.QWidget()
-    tray_icon = pdc.SystemTrayIcon(widget)
-    tray_icon.show()
-
-    check_pager_duty(tray_icon=tray_icon, conf=conf)
+    tray_app = pdc.TrayIcon(widget, conf)
+    tray_app.show()
 
     timer = QtCore.QTimer(widget)
     timer.setInterval(conf['interval'])
-    timer.timeout.connect(lambda t=tray_icon: check_pager_duty(
-        tray_icon=tray_icon, conf=conf))
+    timer.timeout.connect(tray_app.update_incidents)
     timer.start()
 
     sys.exit(app.exec_())
